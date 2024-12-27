5316ecdaaa3a8dfd75c5a4b7ccdc487ab3bed19b
nguyenvanvuong1 <vuong.nguyenvan1@nashtechglobal.com>
2024-12-27 12:58:25 +0700

    setup aks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # azure-infra-tf
+## install azure-cli
+- install
+```brew install azure-cli```
+- login
+```az login```
---
+++ modules/networking/variable.tf
@@ -0,0 +1,30 @@
+variable "name" {
+    type = string
+    default = "sd4776"
+}
+
+variable "location" {
+    type = string
+    default = "southeastasia"
+}
+
+variable "network_address_space" {
+}
+
+variable "aks_subnet_address_prefix" {
+}
+
+variable "aks_subnet_address_name" {
+}
+
+variable "appgw_subnet_address_prefix" {
+}
+
+variable "appgw_subnet_address_name" {
+}
+
+variable "environment" {
+}
+variable "subscription_id" {
+  
+}
